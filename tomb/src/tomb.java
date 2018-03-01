b642b33dc79dac872a9633691e2e1c57e3c26709
TruthP <36498713+TruthP@users.noreply.github.com>
2018-03-01 09:43:07 +0100

    gyak3.

---
+++ tomb/src/tomb.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class tomb {
+	public static void main(String[] args) {
+		int[] tomb = new int[10];
+	
+		Random rand = new Random();
+
+		for (int i = 0; i < tomb.length; i++) {
+			tomb[i] = rand.nextInt(50)+1;
+			System.out.println(tomb[i]);
+}
+}
+		
+}
+
